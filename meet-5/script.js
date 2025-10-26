b6d9c9f58aebb514480e14c8a8ade22db28432e6
MUZAAQI <muzaaqi@yahoo.com>
2025-10-26 11:15:39 +0700

    feat(ui): Create a web protfolio with html and native css

---
+++ meet-5/script.js
@@ -0,0 +1,38 @@
+const getValue = (id) => document.getElementById(id).value;
+
+const modal = document.getElementById("email-modal");
+
+const openModal = () => {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+};
+
+const closeModal = () => {
+  modal.style.display = "none";
+  document.body.style.overflow = "auto";
+};
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  const name = getValue("name");
+  const email = getValue("email");
+  const message = getValue("message");
+  const errorMessage = document.querySelector(".error-message");
+  const confirmSendButton = document.querySelector(".send-button");
+  const mailtoLink = `mailto:muzaaqi@yahoo.com?subject=Contact from ${name}&body=${message}%0D%0A%0D%0AFrom: ${name} (${email})`;
+
+  if (!name || !email || !message) {
+    errorMessage.style.display = "block";
+    return;
+  } else {
+    errorMessage.style.display = "none";
+  }
+
+  openModal();
+
+  confirmSendButton.onclick = () => {
+    window.location.href = mailtoLink;
+    closeModal();
+  };
+};
